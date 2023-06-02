828b8f7b2e58f4344440b1f7a03e8b190863ed8c
MrChill93 <phamthanhtung15693@gmail.com>
2023-06-02 09:59:16 +0700

    update all

---
+++ lib/features/global/data/repo/auth_repo.dart
@@ -0,0 +1,47 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepo {
+  static Future<Either<String, UserCredential>> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final UserCredential userCre = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return Right(userCre);
+    } on FirebaseAuthException catch (err) {
+      return Left(err.message ?? "");
+    } catch (e) {
+      return const Left("");
+    }
+  }
+
+  static Future<Either<String, UserCredential>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final UserCredential userCre = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      return Right(userCre);
+    } on FirebaseAuthException catch (err) {
+      return Left(err.message ?? "");
+    } catch (e) {
+      return const Left("");
+    }
+  }
+
+  static Future<Either<String, bool>> sendPasswordResetEmail({
+    required String email,
+  }) async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      return const Right(true);
+    } on FirebaseAuthException catch (err) {
+      return Left(err.message ?? "");
+    } catch (e) {
+      return const Left("");
+    }
+  }
+}
---
+++ lib/features/home/pages/home_page.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hcm23_03/features/home/cubit/home_cubit.dart';
+import 'package:hcm23_03/features/home/widgets/home_drawer.dart';
+import 'package:hcm23_03/features/task/pages/app_task_pages.dart';
+
+import '../../global/presentation/base_screen/base_screen.dart';
+import '../../task/cubits/today_tasks/today_tasks_cubit.dart';
+import '../../task/data/entities/task.dart';
+import '../../task/pages/today_tasks_page.dart';
+import '../widgets/home_bottom_bar.dart';
+
+class HomePage extends StatefulWidget {
+  static const String routeName = "/HomePage";
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void changePage(int? idx) {
+    context.read<HomeCubit>().changePage(idx);
+  }
+
+  void createNewTask() async {
+    // await Navigator.of(context).pushNamed(
+    //   TaskDetailsPage.routeName,
+    //   arguments: TaskDetailsPageArg(homeCubit: context.read<HomeCubit>()),
+    // );
+  }
+
+  void addNewTaskSuccess(Task task) {}
+
+  // late List<Task> _tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseScreen(builder: (context) {
+      return BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) {
+          return Scaffold(
+            drawer: const HomeDrawer(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: createNewTask,
+              backgroundColor: const Color(0xFFB7ABFD),
+              child: const Icon(Icons.add),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.endDocked,
+            bottomNavigationBar: HomeBottomBar(
+              hasNotch: true,
+              fabLocation: BubbleBottomBarFabLocation.end,
+              opacity: .2,
+              currentIndex: state.currentPage,
+              onTap: changePage,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(16),
+              ),
+              elevation: 8,
+              tilesPadding: const EdgeInsets.symmetric(
+                vertical: 8.0,
+              ),
+              items: const <BubbleBottomBarItem>[
+                BubbleBottomBarItem(
+                  showBadge: true,
+                  // badge: Text("1"),
+                  badgeColor: Colors.deepPurpleAccent,
+                  backgroundColor: Colors.red,
+                  icon: Icon(
+                    Icons.dashboard,
+                    color: Colors.black,
+                  ),
+                  activeIcon: Icon(
+                    Icons.dashboard,
+                    color: Colors.red,
+                  ),
+                  title: Text("All Tasks"),
+                ),
+                BubbleBottomBarItem(
+                    backgroundColor: Colors.deepPurple,
+                    icon: Icon(
+                      Icons.calendar_month,
+                      color: Colors.black,
+                    ),
+                    activeIcon: Icon(
+                      Icons.calendar_month,
+                      color: Colors.deepPurple,
+                    ),
+                    title: Text("T Tasks")),
+                BubbleBottomBarItem(
+                    backgroundColor: Colors.deepPurple,
+                    icon: Icon(
+                      Icons.access_time,
+                      color: Colors.black,
+                    ),
+                    activeIcon: Icon(
+                      Icons.access_time,
+                      color: Colors.deepPurple,
+                    ),
+                    title: Text("")),
+              ],
+            ),
+            body: PageView(
+              physics: const NeverScrollableScrollPhysics(),
+              controller: context.read<HomeCubit>().pageController,
+              children: [
+                const AllTasksPage(),
+                BlocProvider(
+                  create: (context) => TodayTasksCubit()..getTask(context),
+                  child: const TodayTasksPage(),
+                ),
+                Scaffold(
+                  appBar: AppBar(),
+                  body: const Center(
+                    child: Text("Tính năng đang trong quá trình phát triển"),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    });
+  }
+}
